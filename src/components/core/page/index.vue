42df014f5c6eafa0e9e933429c70472d46d877e4
StephenTerror <2684109034@qq.com>
2021-02-09 13:50:00 +0800

    Add files
    Signed-off-by: StephenTerror <2684109034@qq.com>

--- src/components/core/page/index.vue
+++ src/components/core/page/index.vue
@@ -8,6 +8,9 @@
     import G6 from '@antv/g6';
 
     export default {
+        props: {
+            graphData: Object,
+        },
         data() {
             return {
                 graph: null,
@@ -39,8 +42,6 @@
                             radius: 4,
                             lineWidth: 2,
                             fill: '#EDF5FF',
-                            stroke: '#1890ff',
-                            shadowColor: '#1890ff',
                             shadowBlur: 20,
                             shadowOffsetX: 0,
                             shadowOffsetY: 0,
@@ -61,79 +62,8 @@
                     fitViewPadding: 100,
                 });
 
-                // 待渲染数据
-                const data = {
-                    nodes: [
-                        {
-                            id: '1',
-                            label: 'InputBlock',
-                            type: 'rect',
-                            style: {
-                                r: 40,
-                                stroke: '#1890ff',
-                                lineWidth: 5,
-                            },
-                        },
-                        {
-                            id: '2',
-                            label: 'Conv2d',
-                            type: 'rect',
-                            style: {
-                                width: 100,
-                                height: 40,
-                                radius: 6,
-                                lineWidth: 2,
-                            },
-                        },
-                        {
-                            id: '3',
-                            label: 'Conv2d',
-                            type: 'rect',
-                            style: {
-                                rx: 50,
-                                ry: 20,
-                            },
-                        },
-                        {
-                            id: '4',
-                            label: 'OutputBlock',
-                            type: 'rect',
-                            size: [100, 50],
-                            style: {
-                                lineWidth: 2,
-                            },
-                        },
-                    ],
-                    edges: [
-                        {
-                            source: '1',
-                            target: '2',
-                            label: '[32, 3, 28, 28]',
-                            type: 'line',
-                        },
-                        {
-                            source: '1',
-                            target: '3',
-                            label: '[32, 3, 28, 28]',
-                            type: 'line',
-                        },
-                        {
-                            source: '2',
-                            target: '4',
-                            label: '[32, 3, 14, 14]',
-                            type: 'line',
-                        },
-                        {
-                            source: '3',
-                            target: '4',
-                            label: '[32, 3, 14, 14]',
-                            type: 'line',
-                        },
-                    ],
-                };
-
                 this.graph = graph;
-                this.graph.read(data); // 读取数据
+                this.graph.read(this.graphData); // 读取数据
             },
         },
     };
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,16 +1,87 @@
 <template>
   <div>
-    <page/>
+    <page :graph-data="graphData"/>
   </div>
 </template>
 
 <script>
   import page from './core/page'
 
+
   export default {
     name: 'Main',
     props: {
       msg: String
+    },
+    data() {
+      return {
+        graphData: {
+          nodes: [
+            {
+              id: '1',
+              label: 'InputBlock',
+              type: 'rect',
+              style: {
+                rx: 50,
+                ry: 20,
+              },
+            },
+            {
+              id: '2',
+              label: 'Conv2d',
+              type: 'rect',
+              style: {
+                rx: 50,
+                ry: 20,
+              },
+            },
+            {
+              id: '3',
+              label: 'Conv2d',
+              type: 'rect',
+              style: {
+                rx: 50,
+                ry: 20,
+              },
+            },
+            {
+              id: '4',
+              label: 'OutputBlock',
+              type: 'rect',
+              style: {
+                rx: 50,
+                ry: 20,
+              },
+            },
+          ],
+          edges: [
+            {
+              source: '1',
+              target: '2',
+              label: '[32, 3, 28, 28]',
+              type: 'line',
+            },
+            {
+              source: '1',
+              target: '3',
+              label: '[32, 3, 28, 28]',
+              type: 'line',
+            },
+            {
+              source: '2',
+              target: '4',
+              label: '[32, 3, 14, 14]',
+              type: 'line',
+            },
+            {
+              source: '3',
+              target: '4',
+              label: '[32, 3, 14, 14]',
+              type: 'line',
+            },
+          ],
+        },
+      };
     },
     components: {page}
   }
